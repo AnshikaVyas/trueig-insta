d3bc28038dfa66a5f8ad0a90f35289ee4909961e
AnshikaVyas <vyas2004anshi@gmail.com>
2025-12-14 14:11:30 +0530

    feat(frontend): implemented profile update page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -14,7 +14,9 @@ const SignUp = lazy(() => import('./components/User/SignUp'));
 const Login = lazy(() => import('./components/User/Login'));
 const ForgotPassword = lazy(() => import('./components/User/ForgotPassword'));
 const ResetPassword = lazy(() => import('./components/User/ResetPassword'));
+const Update = lazy(() => import('./components/User/Update/Update'));
 const NotFound = lazy(() => import('./components/Errors/NotFound'));
+
 
 function App() {
 
