2c69120821f662c52d5a695d68837105f247d435
AnshikaVyas <vyas2004anshi@gmail.com>
2025-12-14 11:09:30 +0530

    feat(backend): implemented chat functionality

--- backend/app.js
+++ backend/app.js
@@ -12,10 +12,14 @@ app.use('/public', express.static('public'));
 if (process.env.NODE_ENV != "production") {
     require('dotenv').config({ path: 'backend/config/config.env' });
 }
-
+// import routes
 const user = require('./routes/userRoute');
+const post = require('./routes/postRoute');
+const chat = require('./routes/chatRoute');
 
+app.use('/api/v1', post);
 app.use('/api/v1', user);
+app.use('/api/v1', chat);
 
 app.use(errorMiddleware);
 
