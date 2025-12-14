ea97f5c43ac315f7d7e150deacc6aea4c817c668
AnshikaVyas <vyas2004anshi@gmail.com>
2025-12-14 11:55:30 +0530

    feat(backend): implemented message functionality

--- backend/app.js
+++ backend/app.js
@@ -12,15 +12,19 @@ app.use('/public', express.static('public'));
 if (process.env.NODE_ENV != "production") {
     require('dotenv').config({ path: 'backend/config/config.env' });
 }
+
 // import routes
 const user = require('./routes/userRoute');
 const post = require('./routes/postRoute');
 const chat = require('./routes/chatRoute');
+const message = require('./routes/messageRoute');
 
 app.use('/api/v1', post);
 app.use('/api/v1', user);
 app.use('/api/v1', chat);
+app.use('/api/v1', message);
 
+// error middleware
 app.use(errorMiddleware);
 
 module.exports = app;
